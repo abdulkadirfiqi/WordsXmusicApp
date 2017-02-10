85852b6c57413005f43967032bf05a84dab993f2
Abdulkadir Fiqi <abdulkadirfiqi@gmail.com>
2017-02-09 19:26:50 -0800

    this is the last update.

--- app/src/main/java/group3/tcss450/uw/edu/wordsxmusicapp/MainActivity.java
+++ app/src/main/java/group3/tcss450/uw/edu/wordsxmusicapp/MainActivity.java
@@ -1,13 +1,47 @@
 package group3.tcss450.uw.edu.wordsxmusicapp;
 
+import android.app.Activity;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import layout.SearchFragment;
 
+public class MainActivity extends Activity {
+    FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        manager = getFragmentManager();
+
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            if(findViewById(R.id.appboard, new SearchFragment()) != null){
+                manager.beginTransaction()
+                        .add(R.id.appboard,new SearchFragment()).commit();
+            }
+        }
+
+
+    }
+
+    public void setLogin(View v){
+
+    }
+
+    public void setSearch(View v){
+        SearchFragment sch = new SearchFragment();
+        FragmentTransaction searchTran = manager.beginTransaction();
+        searchTran.add(R.id.appboard, sch, "search");
+        searchTran.commit();
+    }
+    public void setClickable(View v){
+
+    }
+    public void setHistory(View v){
+
     }
 }
--- app/src/main/java/layout/ClickableFragment.java
+++ app/src/main/java/layout/ClickableFragment.java
@@ -1,6 +1,5 @@
 package layout;
 
-import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,33 +13,59 @@ import group3.tcss450.uw.edu.wordsxmusicapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SearchFragment.OnFragmentInteractionListener} interface
+ * {@link ClickableFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SearchFragment#newInstance} factory method to
+ * Use the {@link ClickableFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchFragment extends Fragment {
+public class ClickableFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public SearchFragment() {
+    public ClickableFragment() {
         // Required empty public constructor
     }
 
-
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ClickableFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ClickableFragment newInstance(String param1, String param2) {
+        ClickableFragment fragment = new ClickableFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_search, container, false);
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clickable, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -78,7 +103,7 @@ public class SearchFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
